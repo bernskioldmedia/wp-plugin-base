00a9807f16fa9701cca894fb10527d0ef4d683ca
Erik Bernskiold <erik@bernskioldmedia.com>
2021-03-14 10:55:29 +0100

    Add field group command

---
+++ src/Commands/FieldGroupCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BernskioldMedia\WP\PluginBase\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use function Symfony\Component\String\u;
+
+class FieldGroupCommand extends MakeCommand {
+
+	protected static $basePath    = '/src/Fields/';
+	protected static $defaultName = 'make:fieldgroup';
+	protected static $stubName    = 'fieldgroup';
+
+	protected function configure() {
+		parent::configure();
+		$this->addOption( 'namespace', 's', InputOption::VALUE_OPTIONAL, 'The root plugin namespace.', 'NAMESPACE' );
+	}
+
+	protected function getReplacements( InputInterface $input ): array {
+		$name = $input->getArgument( 'name' );
+
+		$args = [
+			'{{ namespace }}' => $input->getOption( 'namespace' ) . '\\Customizer',
+			'{{ class }}'     => u( $name )->camel()->title()->toString(),
+		];
+
+		return $args;
+	}
+
+}
