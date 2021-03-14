86ee6bc8090d047b4258812df3e4316878701eb9
Erik Bernskiold <erik@bernskioldmedia.com>
2021-03-14 10:53:21 +0100

    Add make:customizer

---
+++ src/Commands/CustomizerCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BernskioldMedia\WP\PluginBase\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use function Symfony\Component\String\u;
+
+class CustomizerCommand extends MakeCommand {
+
+	protected static $basePath    = '/src/Customizer/';
+	protected static $defaultName = 'make:customizer';
+	protected static $stubName    = 'customizer';
+
+	protected function configure() {
+		parent::configure();
+		$this->addOption( 'namespace', 's', InputOption::VALUE_OPTIONAL, 'The root plugin namespace.', 'NAMESPACE' )
+		     ->addOption( 'prefix', null, InputOption::VALUE_OPTIONAL, 'The settings prefix.', '' );
+	}
+
+	protected function getReplacements( InputInterface $input ): array {
+		$name = $input->getArgument( 'name' );
+
+		$args = [
+			'{{ namespace }}' => $input->getOption( 'namespace' ) . '\\Customizer',
+			'{{ class }}'     => u( $name )->camel()->title()->toString(),
+			'{{ prefix }}'    => u( $prefix )->snake()->toString(),
+		];
+
+		return $args;
+	}
+
+}
